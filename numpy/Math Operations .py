c8ab1e942730ebaefef6eec83f8de40702bfda08
EesaYousuf <eesayousufyousuf@gmail.com>
2025-06-29 20:58:45 +0530

    operations.py

---
+++ numpy/Math Operations .py
@@ -0,0 +1,8 @@
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+
+print("Add:", a + b)
+print("Subtract:", a - b)
+print("Multiply:", a * b)
+print("Divide:", a / b)
+print("Dot Product:", np.dot(a, b))
